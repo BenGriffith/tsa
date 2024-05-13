134d030ad28bb8e11ef88a06bc3fe33a73b84eab
Ben Griffith <bengriffith@outlook.com>
2024-05-13 22:07:11 +0000

    processed dates check and functionality

--- tsa/scrape_pdf/main.py
+++ tsa/scrape_pdf/main.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 import vertexai
 from bs4 import BeautifulSoup
@@ -7,8 +9,10 @@ from vertexai.preview import generative_models
 
 MODEL = "gemini-1.5-pro-preview-0409"
 BUCKET = "tsa-throughput"
+PROCESSED_DATES = "processed-tsa-dates.json"
 DOMAIN = "https://www.tsa.gov/"
 TSA_URL = "https://www.tsa.gov/foia/readingroom"
+MOST_RECENT_LINKS_COUNT = 5
 
 
 def generate(text, generation_config, safety_settings):
@@ -51,11 +55,22 @@ def extract_date(filename):
     return filename_date
 
 
+def create_processed_dates_blob(blob):
+    blob.upload_from_string('{"processed_dates": []}')
+    json_string = blob.download_as_text()
+    json_data = json.loads(json_string)
+    return json_data
+
+
 def read_tsa_dates_from_gcs():
     client = storage.Client()
     bucket = client.bucket(BUCKET)
-    blobs = bucket.list_blobs()
-    tsa_dates = [extract_date(blob.name) for blob in blobs]
+    blob = bucket.blob(PROCESSED_DATES)
+    if blob.exists():
+        json_string = blob.download_as_text()
+        json_data = json.loads(json_string)
+        return json_data
+    return create_processed_dates_blob(blob)
 
 
 def read_pdf():
@@ -64,7 +79,7 @@ def read_pdf():
     link_elements = soup.find_all("a", class_="foia-reading-link")
 
     links = []
-    for link_element in link_elements[:1]:
+    for link_element in link_elements[:MOST_RECENT_LINKS_COUNT]:
         text = link_element.text
         if "tsa throughput" not in text.lower():
             continue
@@ -73,20 +88,39 @@ def read_pdf():
     return links
 
 
-def write_pdf(links):
-    response = requests.get(f"{DOMAIN}{links[0]}")
+def write_pdf(link, blob_name):
+    response = requests.get(f"{DOMAIN}{link}")
 
     client = storage.Client()
-    bucket = client.bucket("tsa-throughput")
-    blob = bucket.blob("tsa-total-throughput-april-28-2024-to-may-4-2024.pdf")
+    bucket = client.bucket(BUCKET)
+    blob = bucket.blob(blob_name)
     blob.upload_from_string(response.content, content_type="application/pdf")
 
 
 def process_pdf():
-    pass
+    links_to_process = read_pdf()
+    links_processed = read_tsa_dates_from_gcs()
+    links_processed_set = set(links_processed["processed_dates"])
+    links_to_not_process = set()
+
+    for link in links_to_process:
+        first_date, second_date = extract_date(link).split(", ")
+        if first_date in links_processed or second_date in links_processed:
+            links_to_not_process.add(link)
+
+    links_to_process_set = set(links_to_process)
+    links_to_be_processed = links_to_process_set - links_to_not_process
+
+    dates_processed = []
+    for link in links_to_be_processed:
+        blob_name = link.split("/")[-1]
+        write_pdf(link, blob_name)
+
+    # get dates from links to be processed
+    # get dates already processed
+    # check if dates to be processed already exist in file
+    # links processed should be added to processed links
 
 
 if __name__ == "__main__":
-    # links = read_pdf()
-    # write_pdf(links)
-    read_tsa_dates_from_gcs()
+    process_pdf()
