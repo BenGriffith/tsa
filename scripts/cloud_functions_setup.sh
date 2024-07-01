9f5b399e939871b38c7876eb2b3303051cb1d207
Ben Griffith <bengriffith@outlook.com>
2024-07-01 17:16:49 -0500

    renamed directories and terraform resources

--- scripts/cloud_functions_setup.sh
+++ scripts/cloud_functions_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")/.."
-cp requirements.txt tsa/scrape_pdf/
-cp requirements.txt tsa/create_pdf/
+cp requirements.txt tsa/scrape_weekly_pdf/
+cp requirements.txt tsa/publish_dates_from_weekly_pdf/
